cb30fd877d476d8b2dc7a9e97852f67f70f0c0b6
Ryter, Jayson <Jayson.Ryter@aspect.com>
2019-10-26 13:34:00 -0400

    updating startup script

--- mr-popo/start-popo.sh
+++ mr-popo/start-popo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export HUBOT_DISCORD_TOKEN="NjA1MTA1NDkxMjM1OTYyODgy.XbR2oQ.ekMP7rxO14CyW4izJcO3xXPMsmI"
+BOT_TOKEN=$(cat ~/bot_token.txt)
 export HUBOT_MAX_MESSAGE_LENGTH="2000"
 
 ./bin/hubot -a discord
