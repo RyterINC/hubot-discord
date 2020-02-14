009b1a585cea24a40f6e40c3ebfaa28c84ed787d
EC2 Default User <ec2-user@ip-172-31-23-125.ec2.internal>
2020-02-14 01:21:24 +0000

    updating popo

--- mr-popo/start-popo.sh
+++ mr-popo/start-popo.sh
@@ -3,5 +3,6 @@
 export HUBOT_DISCORD_TOKEN=$(cat ~/bot_token.txt)
 export HUBOT_MAX_MESSAGE_LENGTH="2000"
 
-./bin/hubot -a discord
+nohup $(./bin/hubot -a discord) > popo.log 2>&1 &
+echo $! > save_pid.txt
 
